05001c24d833053b180d93682439f09bbeed44ab
Baris Gungor <gungorbaris41@gmail.com>
2023-05-11 13:44:45 +0300

    layout regulation

--- app/src/main/java/com/barisgungorr/MainActivity2.kt
+++ app/src/main/java/com/barisgungorr/MainActivity2.kt
@@ -3,10 +3,16 @@ package com.barisgungorr
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.barisgungorr.polatcatch.R
+import com.barisgungorr.polatcatch.databinding.ActivityMain2Binding
+import com.barisgungorr.polatcatch.databinding.ActivityMainBinding
 
 class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
     }
 }
--- app/src/main/java/com/barisgungorr/polatcatch/MainActivity.kt
+++ app/src/main/java/com/barisgungorr/polatcatch/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.barisgungorr.polatcatch
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.barisgungorr.MainActivity2
+import com.barisgungorr.polatcatch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
     }
     fun change(view: View) {
+    val intent = Intent(this@MainActivity,MainActivity2::class.java)
+        startActivity(intent)
+        finish()
 
     }
 }
