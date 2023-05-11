eb8e841cb68ee7b14f03500c1c17d2144ec12b01
Baris Gungor <gungorbaris41@gmail.com>
2023-05-11 12:43:35 +0300

    project update

---
+++ app/src/main/java/com/barisgungorr/MainActivity2.kt
@@ -0,0 +1,12 @@
+package com.barisgungorr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.barisgungorr.polatcatch.R
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
