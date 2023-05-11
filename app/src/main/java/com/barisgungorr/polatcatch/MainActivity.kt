46e4ee749107ded1c99d9a9eaeb49853a36e94a2
Baris Gungor <gungorbaris41@gmail.com>
2023-05-11 13:25:08 +0300

    welcome to Screen complete

--- app/src/main/java/com/barisgungorr/polatcatch/MainActivity.kt
+++ app/src/main/java/com/barisgungorr/polatcatch/MainActivity.kt
@@ -2,10 +2,14 @@ package com.barisgungorr.polatcatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun change(view: View) {
+
     }
 }
