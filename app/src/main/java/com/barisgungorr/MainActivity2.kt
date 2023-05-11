f89ea3dbf290c2c5abf319a034242d64157e79c5
Baris Gungor <gungorbaris41@gmail.com>
2023-05-11 14:36:53 +0300

    lose the ımAges

--- app/src/main/java/com/barisgungorr/MainActivity2.kt
+++ app/src/main/java/com/barisgungorr/MainActivity2.kt
@@ -3,13 +3,16 @@ package com.barisgungorr
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import com.barisgungorr.polatcatch.R
 import com.barisgungorr.polatcatch.databinding.ActivityMain2Binding
 import com.barisgungorr.polatcatch.databinding.ActivityMainBinding
+import java.util.Random
 
 class MainActivity2 : AppCompatActivity() {
     private lateinit var binding: ActivityMain2Binding
     var score = 0 // bu skora başka fonksiyonlardan da ulaşmak isteyebilirim
+    var imageArray = ArrayList<ImageView>() // ImageViewlerimi eklemek için bir arrayList oluşturduk
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,9 +20,30 @@ class MainActivity2 : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        imageArray.add(binding.imageView)
+        imageArray.add(binding.imageView2)
+        imageArray.add(binding.imageView3)
+        imageArray.add(binding.imageView4)
+        imageArray.add(binding.imageView5)
+        imageArray.add(binding.imageView6)
+        imageArray.add(binding.imageView7)
+        imageArray.add(binding.imageView8)
+        imageArray.add(binding.imageView9)
+
     }
-    fun increaseScore(view: View) {
+    fun increaseScore(view: View) {  // we added our func that will increase the every time we click on the photo
     score = score + 1
         binding.textViewScore.text = "Score: ${score}"
     }
+    fun hideImages(){
+    for(image in imageArray) {
+        image.visibility = View.INVISIBLE  // görselleri kaybettik
+    }
+        val random = Random()
+        val randomIndex = random.nextInt(9) // 0'dan 9 a kadar random bir ımageVıew açılacak uygulamayı her açtığımızda
+        imageArray[randomIndex].visibility = View.VISIBLE
+
+
+    }
+
 }
